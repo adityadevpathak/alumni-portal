838616f1f662cba7c1aef08faaba72c5d825243f
adityadevpathak <adityadevpathak0@gmail.com>
2026-02-08 15:29:37 +0530

    Update app run configuration for deployment

--- app.py
+++ app.py
@@ -202,4 +202,6 @@ def init_sample():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
