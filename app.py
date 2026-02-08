f1256ebc82fb00b55da654975cd334a4bc61208f
adityadevpathak <adityadevpathak0@gmail.com>
2026-02-08 17:53:41 +0530

    Fix indentation error in __main__ block

--- app.py
+++ app.py
@@ -197,10 +197,11 @@ def init_sample():
         return 'Database already initialized.'
 
 # ---------------------------
+# ---------------------------
 # Run App
 # ---------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-  port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port) 
+
+    app.run()
