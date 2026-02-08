eb0f933bd265d74d21232af25763c04a07109396
adityadevpathak <adityadevpathak0@gmail.com>
2026-02-08 16:56:07 +0530

    Fix duplicate __main__ block and indentation

--- app.py
+++ app.py
@@ -202,6 +202,5 @@ def init_sample():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
-
+  port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port) 
